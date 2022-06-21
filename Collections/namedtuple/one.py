30905cc084518e92b48f6e59ab2937691cafe1ac
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-06-21 22:51:47 +0600

    regular tuple complex of readblity

---
+++ Collections/namedtuple/one.py
@@ -0,0 +1,13 @@
+# Create a 2D point as tuple
+point = (2, 3)
+
+#print the coordinate (x, y)
+print(point)
+# Access coordinate (x)
+print(point[0])
+# Access coordinate (y)
+print(point[1])
+
+# Try to update coordinate value
+
+point[0] = 3
