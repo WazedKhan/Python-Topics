a18cea26357dddfa47d6bf7b7c91ce9068bd5eee
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-18 21:01:24 +0600

    applied map

--- Filter Function/evenNumbers.py
+++ Filter Function/evenNumbers.py
@@ -2,6 +2,6 @@
 
 numbers = [1, 3, 10, 45, 6, 50]
 
-# Using filter function
-evenNumbers = filter(lambda x: x % 2 == 0, numbers)
-print(list(evenNumbers))
+sqrEven =  list(map(lambda x: x**2, filter(lambda x: x % 2 == 0, numbers)))
+
+print(sqrEven)
