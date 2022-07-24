89f9f3af8317346a3abce744801f0545d7d962bf
Wazed Khan <51821594+WazedKhan@users.noreply.github.com>
2022-07-24 16:33:07 +0600

    value assignment

---
+++ Variables/assgin.py
@@ -0,0 +1,9 @@
+# single assignmnet
+x = 10
+
+# Assign Value to Multiple Variables
+x, y, z = 10, 20, 30
+
+# assign the same value to multiple variables in one line
+# chain assignment
+x = y = z = 40
